91653a2cc0d7eff1de9fbf75dc693b248086ba71
djordjeviclazar <djordjevic.lazar@elfak.rs>
2022-01-20 23:19:38 +0100

    Dodat Seed

---
+++ Seed/TwoRandPlayersSeed.sql
@@ -0,0 +1,5 @@
+INSERT INTO `nsi`.`players` (`FirstName`,`LastName`,`Fide_Id`,`EloRating`,`Coefficient`,`Title`,`Country`,`Sex`,`Birth`) 
+		VALUES('John', 'Doe', NULL, 1500, 30, 12, 'England', 'Male', '2005-04-23 00:00:');
+		
+INSERT INTO `nsi`.`players` (`FirstName`,`LastName`,`Fide_Id`,`EloRating`,`Coefficient`,`Title`,`Country`,`Sex`,`Birth`) 
+		VALUES('Jane', 'Doe', NULL, 1700, 15, 11, 'England', 'Female', '2007-01-10 00:00');
